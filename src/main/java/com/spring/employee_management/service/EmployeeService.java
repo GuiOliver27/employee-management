32c3d6aa6fb47202379d0a5f28792a1349829042
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-12-29 20:03:37 -0400

    Feat: Classes UserRepository e EmployeeService criadas

---
+++ src/main/java/com/spring/employee_management/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.spring.employee_management.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.spring.employee_management.model.Employee;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
