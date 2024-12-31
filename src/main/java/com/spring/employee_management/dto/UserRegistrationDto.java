24ee411a8d968debe117d07c7ca98df6beed678c
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-12-30 20:19:45 -0400

    Feat: Classes UserService e UserRegistrationDto criadas

---
+++ src/main/java/com/spring/employee_management/dto/UserRegistrationDto.java
@@ -0,0 +1,21 @@
+package com.spring.employee_management.dto;
+
+@SuppressWarnings("unused")
+public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserRegistrationDto() {
+
+    }
+
+    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
